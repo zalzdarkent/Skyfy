9d7dfab14cfb63c2bceaa571338884e1e8daa88b
Zalzdarkent <you@example.com>
2023-02-13 10:05:40 +0700

    commit 2

--- forms/koneksi.php
+++ forms/koneksi.php
@@ -9,15 +9,16 @@ $tier = $_POST['tier'];
 $no_hp = $_POST['hp'];
 $persetujuan = $_POST['agreement'];
 
-if (!isset($persetujuan)) {
-    echo "<script>alert('Anda harus menyetujui syarat dan ketentuan!');</script>";
-    exit();
-}
-
-
 // $query = "INSERT INTO form values ($nama_lengkap, $umur, $jenis_kelamin, $asal_kota, $alasan_masuk)";
 mysqli_query($koneksi, "INSERT INTO member values ('', '$name', '$asal_kota', '$umur', '$jenis_kelamin', '$tier', '$no_hp', '$persetujuan')");
 
-if ($koneksi) {
-    header("location: ../popup.html");
+
+if (!isset($_POST['agreement'])) {
+    echo "<script>
+            alert('Anda harus menyetujui regulasi yang ada');
+            window.history.back();
+          </script>";
+    exit;
+} else {
+    header("location: popup.html");
 }
